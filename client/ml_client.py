24076fb8d7dc0d07de424c04c71e88e7edb27a67
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2022-10-25 13:58:57 +0200

    create a basic ml client

---
+++ client/ml_client.py
@@ -0,0 +1,69 @@
+from requests import Session
+from requests.auth import HTTPBasicAuth, HTTPDigestAuth
+import logging
+
+
+class MLClient:
+
+    def __init__(self, protocol: str = "http", host: str = "localhost", port: int = 8002,
+                auth: str = "basic", username: str = "admin", password: str = "admin"):
+        self.protocol = protocol
+        self.host = host
+        self.port = port
+        self.auth = HTTPBasicAuth(username, password) if auth == "basic" else HTTPDigestAuth(username, password)
+        self.username = username
+        self.password = password
+        self.base_url = f'{protocol}://{host}:{port}'
+        self.sess = None
+        self.logger = logging.getLogger("ml-client")
+
+    def __enter__(self):
+        self.init_conn()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.disconnect()
+        return None
+
+    def init_conn(self):
+        self.logger.debug("Initiating a connection")
+        self.sess = Session()
+
+    def disconnect(self):
+        if self.sess:
+            self.logger.debug("Closing a connection")
+            self.sess.close()
+            self.sess = None
+
+    def get(self, endpoint: str, params: dict = None, headers: dict = None):
+        if self.sess:
+            url = self.base_url + endpoint
+            if not headers:
+                headers = {}
+            if not params:
+                params = {}
+            return self.sess.get(url, auth=self.auth, params=params, headers=headers)
+
+    def post(self, endpoint: str, params: dict = None, headers: dict = None, body=None):
+        if self.sess:
+            url = self.base_url + endpoint
+            if not headers:
+                headers = {}
+            if not params:
+                params = {}
+            if headers["content-type"] == "application/json":
+                return self.sess.post(url, auth=self.auth, params=params, headers=headers, json=body)
+            else:
+                return self.sess.post(url, auth=self.auth, params=params, headers=headers, data=body)
+
+    def put(self, endpoint: str, params: dict = None, headers: dict = None, body=None):
+        if self.sess:
+            url = self.base_url + endpoint
+            if not headers:
+                headers = {}
+            if not params:
+                params = {}
+            if headers["content-type"] == "application/json":
+                return self.sess.put(url, auth=self.auth, params=params, headers=headers, json=body)
+            else:
+                return self.sess.put(url, auth=self.auth, params=params, headers=headers, data=body)

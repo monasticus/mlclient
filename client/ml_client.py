c57305c12e2d6564796d74650a9dd7ddd95dedf9
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2022-10-26 17:32:26 +0200

    make ml_client fields private and declare returning type

--- client/ml_client.py
+++ client/ml_client.py
@@ -2,7 +2,7 @@ from client import constants
 from client.rest_resources.client_api.eval_call import EvalCall
 from client.rest_resources.management_api.logs_call import LogsCall
 from client.rest_resources.resource_call import ResourceCall
-from requests import Session
+from requests import Session, Response
 from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 import logging
 
@@ -11,72 +11,75 @@ class MLClient:
 
     def __init__(self, protocol: str = "http", host: str = "localhost", port: int = 8002,
                 auth: str = "basic", username: str = "admin", password: str = "admin"):
-        self.protocol = protocol
-        self.host = host
-        self.port = port
-        self.auth = HTTPBasicAuth(username, password) if auth == "basic" else HTTPDigestAuth(username, password)
-        self.username = username
-        self.password = password
-        self.base_url = f'{protocol}://{host}:{port}'
-        self.sess = None
-        self.logger = logging.getLogger("ml-client")
+        self.__protocol = protocol
+        self.__host = host
+        self.__port = port
+        self.__auth = HTTPBasicAuth(username, password) if auth == "basic" else HTTPDigestAuth(username, password)
+        self.__username = username
+        self.__password = password
+        self.__base_url = f'{protocol}://{host}:{port}'
+        self.__sess = None
+        self.__logger = logging.getLogger("ml-client")
 
     def __enter__(self):
-        self.init_conn()
+        self.connect()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.disconnect()
         return None
 
-    def init_conn(self):
-        self.logger.debug("Initiating a connection")
-        self.sess = Session()
+    def connect(self):
+        self.__logger.debug("Initiating a connection")
+        self.__sess = Session()
 
     def disconnect(self):
-        if self.sess:
-            self.logger.debug("Closing a connection")
-            self.sess.close()
-            self.sess = None
+        if self.__sess:
+            self.__logger.debug("Closing a connection")
+            self.__sess.close()
+            self.__sess = None
 
-    def get(self, endpoint: str, params: dict = None, headers: dict = None):
-        if self.sess:
-            url = self.base_url + endpoint
+    def is_connected(self):
+        return self.__sess is not None
+
+    def get(self, endpoint: str, params: dict = None, headers: dict = None) -> Response:
+        if self.__sess:
+            url = self.__base_url + endpoint
             if not headers:
                 headers = {}
             if not params:
                 params = {}
-            return self.sess.get(url, auth=self.auth, params=params, headers=headers)
+            return self.__sess.get(url, auth=self.__auth, params=params, headers=headers)
 
-    def post(self, endpoint: str, params: dict = None, headers: dict = None, body=None):
-        if self.sess:
-            url = self.base_url + endpoint
-            if not headers:
-                headers = {}
-            if not params:
-                params = {}
-            if headers[constants.HEADER_CONTENT_TYPE] == constants.HEADER_CONTENT_TYPE_JSON:
-                return self.sess.post(url, auth=self.auth, params=params, headers=headers, json=body)
-            else:
-                return self.sess.post(url, auth=self.auth, params=params, headers=headers, data=body)
-
-    def put(self, endpoint: str, params: dict = None, headers: dict = None, body=None):
-        if self.sess:
-            url = self.base_url + endpoint
+    def post(self, endpoint: str, params: dict = None, headers: dict = None, body=None) -> Response:
+        if self.__sess:
+            url = self.__base_url + endpoint
             if not headers:
                 headers = {}
             if not params:
                 params = {}
             if headers[constants.HEADER_CONTENT_TYPE] == constants.HEADER_CONTENT_TYPE_JSON:
-                return self.sess.put(url, auth=self.auth, params=params, headers=headers, json=body)
+                return self.__sess.post(url, auth=self.__auth, params=params, headers=headers, json=body)
             else:
-                return self.sess.put(url, auth=self.auth, params=params, headers=headers, data=body)
+                return self.__sess.post(url, auth=self.__auth, params=params, headers=headers, data=body)
+
+    def put(self, endpoint: str, params: dict = None, headers: dict = None, body=None) -> Response:
+        if self.__sess:
+            url = self.__base_url + endpoint
+            if not headers:
+                headers = {}
+            if not params:
+                params = {}
+            if headers[constants.HEADER_CONTENT_TYPE] == constants.HEADER_CONTENT_TYPE_JSON:
+                return self.__sess.put(url, auth=self.__auth, params=params, headers=headers, json=body)
+            else:
+                return self.__sess.put(url, auth=self.__auth, params=params, headers=headers, data=body)
 
 
 class MLResourceClient(MLClient):
 
     def eval(self, xquery: str = None, javascript: str = None, variables: dict = None,
-             database: str = None, txid: str = None):
+             database: str = None, txid: str = None) -> Response:
         call = EvalCall(xquery=xquery,
                         javascript=javascript,
                         variables=variables,
@@ -85,7 +88,7 @@ class MLResourceClient(MLClient):
         return self.call(call)
 
     def get_logs(self, data_format: str = None, filename: str = None, host: str = None,
-                 start_time: str = None, end_time: str = None, regex: str = None):
+                 start_time: str = None, end_time: str = None, regex: str = None) -> Response:
         call = LogsCall(data_format=data_format,
                         filename=filename,
                         host=host,
@@ -94,7 +97,7 @@ class MLResourceClient(MLClient):
                         regex=regex)
         return self.call(call)
 
-    def call(self, call: ResourceCall):
+    def call(self, call: ResourceCall) -> Response:
         method = call.method()
         if method == constants.METHOD_GET:
             return self.get(endpoint=call.endpoint(),

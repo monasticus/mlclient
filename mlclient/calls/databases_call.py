cb9da34ead008b25b8cefa1be35358c09cdb5584
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2022-11-02 16:56:24 +0100

    add documentation to the databases call class

--- mlclient/calls/databases_call.py
+++ mlclient/calls/databases_call.py
@@ -1,12 +1,32 @@
 import json
-
-from dateutil import parser
+from typing import Union
 
 from mlclient import constants, exceptions, utils
 from mlclient.calls import ResourceCall
 
 
 class DatabasesCall(ResourceCall):
+    """
+    A ResourceCall implementation representing a single request to the /manage/v2/databases REST Resource
+
+    This resource address
+    - (GET) returns a summary of the databases in the cluster.
+    - (POST) creates a new database in the cluster.
+
+    Documentation of the REST Resource API:
+        - https://docs.marklogic.com/REST/GET/manage/v2/databases
+        - https://docs.marklogic.com/REST/POST/manage/v2/databases
+
+    Attributes
+    ----------
+    ENDPOINT
+        a static constant storing the Databases endpoint value
+
+    Methods
+    -------
+    All public methods are inherited from the ResourceCall abstract class.
+    This class implements the endpoint() abstract method to return an endpoint for the specific call.
+    """
 
     ENDPOINT = "/manage/v2/databases"
 
@@ -16,7 +36,20 @@ class DatabasesCall(ResourceCall):
     __SUPPORTED_FORMATS = ["xml", "json", "html"]
     __SUPPORTED_VIEWS = ["describe", "default", "metrics", "package", "schema", "properties-schema"]
 
-    def __init__(self, method: str, data_format: str = "xml", view: str = "default", body=None):
+    def __init__(self, method: str, data_format: str = "xml", view: str = "default", body: Union[str, dict] = None):
+        """
+        Parameters
+        ----------
+        method : str
+            A request method.
+        data_format : str
+            The format of the returned data. Can be either html, json, or xml (default).
+        view : str
+            A specific view of the returned data. Can be schema, metrics, package, or default.
+        body : Union[str, dict]
+            A database properties in XML or JSON format.
+        """
+
         data_format = data_format if data_format is not None else "xml"
         if data_format not in DatabasesCall.__SUPPORTED_FORMATS:
             raise exceptions.WrongParameters("The supported formats are: " + ", ".join(DatabasesCall.__SUPPORTED_FORMATS))
@@ -31,6 +64,14 @@ class DatabasesCall(ResourceCall):
             raise exceptions.WrongParameters("Method not allowed: the supported methods are GET and POST!")
 
     def endpoint(self):
+        """Implementation of an abstract method returning an endpoint for the Databases call
+
+        Returns
+        -------
+        str
+            an Databases call endpoint
+        """
+
         return DatabasesCall.ENDPOINT
 
     def __init_get(self, data_format: str, view: str):
@@ -49,53 +90,3 @@ class DatabasesCall(ResourceCall):
         self.add_param(DatabasesCall.__FORMAT_PARAM, data_format)
         self.add_header(constants.HEADER_NAME_CONTENT_TYPE, content_type)
         self.set_body(body)
-
-
-class LogsCall(ResourceCall):
-
-    ENDPOINT = "/manage/v2/logs"
-
-    __FORMAT_PARAM = "format"
-    __FILENAME_PARAM = "filename"
-    __HOST_PARAM = "host"
-    __START_PARAM = "start"
-    __END_PARAM = "end"
-    __REGEX_PARAM = "regex"
-
-    def __init__(self, filename: str, data_format: str = "html", host: str = None,
-                 start_time: str = None, end_time: str = None, regex: str = None):
-        data_format = data_format if data_format is not None else "html"
-        LogsCall.__validate_params(data_format, start_time, end_time)
-
-        super().__init__(accept=utils.get_accept_header_for_format(data_format))
-        self.add_param(LogsCall.__FORMAT_PARAM, data_format)
-        self.add_param(LogsCall.__FILENAME_PARAM, filename)
-        self.add_param(LogsCall.__HOST_PARAM, host)
-        self.add_param(LogsCall.__START_PARAM, LogsCall.__reformat_datetime_param(start_time))
-        self.add_param(LogsCall.__END_PARAM, LogsCall.__reformat_datetime_param(end_time))
-        self.add_param(LogsCall.__REGEX_PARAM, regex)
-
-    def endpoint(self):
-        return LogsCall.ENDPOINT
-
-    @staticmethod
-    def __validate_params(data_format: str, start_time: str, end_time: str):
-        if data_format and data_format not in ["xml", "json", "html"]:
-            raise exceptions.WrongParameters("The supported formats are xml, json or html!")
-        LogsCall.__validate_datetime_param("start", start_time)
-        LogsCall.__validate_datetime_param("end", end_time)
-
-    @staticmethod
-    def __validate_datetime_param(param_name: str, param_value: str):
-        try:
-            if param_value:
-                parser.parse(param_value)
-        except ValueError:
-            raise exceptions.WrongParameters(f"The {param_name} parameter is not a dateTime value!")
-
-    @staticmethod
-    def __reformat_datetime_param(datetime_param: str):
-        if datetime_param:
-            return parser.parse(datetime_param).strftime("%Y-%m-%dT%H:%M:%S")
-        else:
-            return datetime_param

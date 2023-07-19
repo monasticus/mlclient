655f38b6a8336b54ebcbd8de9d98fe50a69811c3
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-07-19 12:07:03 +0200

    mlclient #4 - complete documentation for MLResourceClient class and move it to ML Client module

--- mlclient/__init__.py
+++ mlclient/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
-from .ml_client import MLClient
-from .ml_resource_client import MLResourceClient
+from .ml_client import MLClient, MLResourceClient
 
 __version__ = "0.1.0"
 __all__ = ["MLClient", "MLResourceClient"]

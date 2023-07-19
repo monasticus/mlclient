12363e3fb3131c5cd46acae72942324b16c75141
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-07-19 15:55:30 +0200

    mlclient #4 - build docstrings for mlclient exceptions module

--- mlclient/exceptions.py
+++ mlclient/exceptions.py
@@ -1,9 +1,25 @@
+"""The ML Client Exceptions module.
+
+It contains all custom exceptions related to ML Client:
+    * WrongParametersError
+        A custom Exception class for wrong parameters.
+    * UnsupportedFormatError
+        A custom Exception class for an unsupported format.
+"""
 from __future__ import annotations
 
 
 class WrongParametersError(Exception):
+    """A custom Exception class for wrong parameters.
+
+    Raised when attempting to call a REST Resource with incorrect parameters.
+    """
     pass
 
 
 class UnsupportedFormatError(Exception):
+    """A custom Exception class for an unsupported format.
+
+    Raised when getting an accept header for a format.
+    """
     pass

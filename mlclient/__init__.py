8148f0d351517192a090236a1949e1c324f7b25a
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-07-29 14:44:44 +0200

    mlclient #4 - finalize mlclient package

--- mlclient/__init__.py
+++ mlclient/__init__.py
@@ -1,3 +1,26 @@
+"""The ML Client package.
+
+The root package of Python API to manage MarkLogic instance. It contains the most
+generic modules:
+* ml_client
+    The ML Client module.
+* constants
+    The ML Client Constants module.
+* exceptions
+    The ML Client Exceptions module.
+* utils
+    The ML Client Utils module.
+
+This package exports the following classes:
+* MLClient
+    A low-level class used to send simple HTTP requests to a MarkLogic instance.
+* MLResourceClient
+    An MLClient subclass supporting internal REST Resources of the MarkLogic server.
+
+Examples
+--------
+from mlclient import Document, DocumentType, Metadata
+"""
 from __future__ import annotations
 
 from .ml_client import MLClient, MLResourceClient

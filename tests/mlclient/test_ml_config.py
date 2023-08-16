931c47b40177f6c31e12db0821f01a2d196599c3
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-08-16 11:30:08 +0200

    mlclient #12 - refactor unit tests

--- tests/mlclient/test_ml_config.py
+++ tests/mlclient/test_ml_config.py
@@ -1,4 +1,5 @@
 from mlclient import AuthMethod, MLConfiguration
+from mlclient.ml_config import MLAppServerConfiguration
 
 RESOURCES_PATH = "tests/resources/test-ml-config"
 
@@ -6,34 +7,40 @@ RESOURCES_PATH = "tests/resources/test-ml-config"
 def test_from_file():
     path = f"{RESOURCES_PATH}/test-from-file.yaml"
     config = MLConfiguration.from_file(path)
-    assert config.app_name == "my-marklogic-app"
-    assert config.host == "localhost"
-    assert config.username == "admin"
-    assert config.password == "admin"
-    assert config.protocol == "http"
-    assert len(config.app_servers) == 5
-    assert config.app_servers[0].model_dump() == {
-        "identifier": "manage",
-        "port": 8002,
-        "auth": AuthMethod.BASIC,
+    assert config.model_dump() == {
+        "app_name": "my-marklogic-app",
+        "host": "localhost",
+        "username": "admin",
+        "password": "admin",
+        "protocol": "http",
+        "app_servers": [
+            {
+                "identifier": "manage",
+                "port": 8002,
+                "auth": AuthMethod.BASIC,
+            },
+            {
+                "identifier": "content",
+                "port": 8100,
+                "auth": AuthMethod.BASIC,
+            },
+            {
+                "identifier": "modules",
+                "port": 8101,
+                "auth": AuthMethod.BASIC,
+            },
+            {
+                "identifier": "schemas",
+                "port": 8102,
+                "auth": AuthMethod.BASIC,
+            },
+            {
+                "identifier": "test",
+                "port": 8103,
+                "auth": AuthMethod.BASIC,
+            },
+        ],
     }
-    assert config.app_servers[1].model_dump() == {
-        "identifier": "content",
-        "port": 8100,
-        "auth": AuthMethod.BASIC,
-    }
-    assert config.app_servers[2].model_dump() == {
-        "identifier": "modules",
-        "port": 8101,
-        "auth": AuthMethod.BASIC,
-    }
-    assert config.app_servers[3].model_dump() == {
-        "identifier": "schemas",
-        "port": 8102,
-        "auth": AuthMethod.BASIC,
-    }
-    assert config.app_servers[4].model_dump() == {
-        "identifier": "test",
-        "port": 8103,
-        "auth": AuthMethod.BASIC,
-    }
+    assert isinstance(config, MLConfiguration)
+    assert all(isinstance(app_server_config, MLAppServerConfiguration)
+               for app_server_config in config.app_servers)

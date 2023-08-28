d5fba9a3e604a6c54758e93d02ec5b10c34e08d7
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-08-28 16:40:03 +0200

    mlclient #52 - add documentation to clients package

--- mlclient/clients/logs_client.py
+++ mlclient/clients/logs_client.py
@@ -1,3 +1,11 @@
+"""The ML Logs Client module.
+
+It exports high-level classes to easily read MarkLogicLogs:
+    * LogsClient
+        An MLResourceClient calling /manage/v2/logs endpoint.
+    * LogType
+        An enumeration class representing MarkLogic log types.
+"""
 from __future__ import annotations
 
 from enum import Enum
@@ -9,12 +17,19 @@ from mlclient.exceptions import MarkLogicError
 
 
 class LogType(Enum):
+    """An enumeration class representing MarkLogic log types."""
+
     ERROR = "ErrorLog"
     ACCESS = "AccessLog"
     REQUEST = "RequestLog"
 
 
 class LogsClient(MLResourceClient):
+    """An MLResourceClient calling /manage/v2/logs endpoint.
+
+    It is a high-level class parsing MarkLogic response and extracting logs from
+    the server.
+    """
 
     def get_logs(
             self,
@@ -25,6 +40,33 @@ class LogsClient(MLResourceClient):
             regex: str | None = None,
             host: str | None = None,
     ) -> Iterator[dict]:
+        """Return logs from a MarkLogic server.
+
+        Parameters
+        ----------
+        app_server_port : int
+            A port with logs to retrieve
+        log_type : LogType, default LogType.ERROR
+            A log type
+        start_time : str | None = None
+            A start time to search error logs
+        end_time : str | None = None
+            An end time to search error logs
+        regex : str | None = None
+            A regex to search error logs
+        host : str | None = None
+            A host name with logs to retrieve
+
+        Returns
+        -------
+        Iterator[dict]
+            A log details generator.
+
+        Raises
+        ------
+        MarkLogicError
+            If MarkLogic returns an error (most likely XDMP-NOSUCHHOST)
+        """
         call = self._get_call(
             app_server_port=app_server_port,
             log_type=log_type,
@@ -42,12 +84,37 @@ class LogsClient(MLResourceClient):
     @staticmethod
     def _get_call(
             app_server_port: int,
-            log_type: LogType = LogType.ERROR,
+            log_type: LogType,
             start_time: str | None = None,
             end_time: str | None = None,
             regex: str | None = None,
             host: str | None = None,
     ) -> LogsCall:
+        """Prepare a LogsCall instance.
+
+        It initializes a LogsCall instance with adjusted parameters. When log type
+        is not ERROR, search params are ignored: start_time, end_time and regex.
+
+        Parameters
+        ----------
+        app_server_port : int
+            A port with logs to retrieve
+        log_type : LogType
+            A log type
+        start_time : str | None = None
+            A start time to search error logs
+        end_time : str | None = None
+            An end time to search error logs
+        regex : str | None = None
+            A regex to search error logs
+        host : str | None = None
+            A host name with logs to retrieve
+
+        Returns
+        -------
+        LogsCall
+            A prepared LogsCall instance
+        """
         file_name = f"{app_server_port}_{log_type.value}.txt"
         params = {
             "filename": file_name,
@@ -68,6 +135,20 @@ class LogsClient(MLResourceClient):
             log_type: LogType,
             resp_json: dict,
     ) -> Iterator[dict]:
+        """Parse MarkLogic logs depending on their type.
+
+        Parameters
+        ----------
+        log_type : LogType
+            A log type
+        resp_json : dict
+            A JSON response from an MarkLogic server
+
+        Returns
+        -------
+        Iterator[dict]
+            A log details generator.
+        """
         logfile = resp_json["logfile"]
         if log_type == LogType.ERROR:
             logs = logfile.get("log", ())

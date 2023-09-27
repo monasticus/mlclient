0d2139d6a490b6e364ee0726b547fb082f088d0c
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-09-27 14:55:07 +0200

    mlclient #101 - create documents delete call

---
+++ tests/mlclient/calls/test_documents_delete_call.py
@@ -0,0 +1,96 @@
+import pytest
+
+from mlclient import exceptions
+from mlclient.calls import DocumentsDeleteCall
+
+
+@pytest.fixture()
+def default_documents_get_call():
+    """Returns an DocumentsDeleteCall instance"""
+    return DocumentsDeleteCall(uri="/a.xml")
+
+
+def test_validation_category_param():
+    with pytest.raises(exceptions.WrongParametersError) as err:
+        DocumentsDeleteCall(
+            uri="/a.xml",
+            category="X")
+
+    expected_msg = ("The supported categories are: "
+                    "content, metadata, metadata-values, collections, "
+                    "permissions, properties, quality")
+    assert err.value.args[0] == expected_msg
+
+
+def test_endpoint(default_documents_get_call):
+    assert default_documents_get_call.endpoint == "/v1/documents"
+
+
+def test_method(default_documents_get_call):
+    assert default_documents_get_call.method == "DELETE"
+
+
+def test_parameters_single_uri():
+    assert DocumentsDeleteCall(uri="/a.xml").params == {
+        "uri": "/a.xml",
+    }
+
+
+def test_parameters_multiple_uris():
+    assert DocumentsDeleteCall(uri=["/a.xml", "/b.xml"]).params == {
+        "uri": ["/a.xml", "/b.xml"],
+    }
+
+
+def test_headers(default_documents_get_call):
+    assert default_documents_get_call.headers == {}
+
+
+def test_body(default_documents_get_call):
+    assert default_documents_get_call.body is None
+
+
+def test_fully_parametrized_call_for_single_uri():
+    call = DocumentsDeleteCall(
+        uri="/a.xml",
+        database="Documents",
+        category="content",
+        txid="transaction",
+        temporal_collection="Entity-collection",
+        system_time="2023-09-09T01:01:01.000Z",
+        wipe_temporal=True)
+    assert call.method == "DELETE"
+    assert call.headers == {}
+    assert call.params == {
+        "uri": "/a.xml",
+        "database": "Documents",
+        "category": "content",
+        "txid": "transaction",
+        "temporal-collection": "Entity-collection",
+        "system-time": "2023-09-09T01:01:01.000Z",
+        "result": "wipe",
+    }
+    assert call.body is None
+
+
+def test_fully_parametrized_call_for_multiple_uris():
+    call = DocumentsDeleteCall(
+        uri=["/a.xml", "/b.xml"],
+        database="Documents",
+        category="content",
+        txid="transaction",
+        temporal_collection="Entity-collection",
+        system_time="2023-09-09T01:01:01.000Z",
+        wipe_temporal=True)
+    assert call.method == "DELETE"
+    assert call.headers == {}
+    assert call.params == {
+        "uri": ["/a.xml", "/b.xml"],
+        "database": "Documents",
+        "category": "content",
+        "txid": "transaction",
+        "temporal-collection": "Entity-collection",
+        "system-time": "2023-09-09T01:01:01.000Z",
+        "result": "wipe",
+    }
+    assert call.body is None

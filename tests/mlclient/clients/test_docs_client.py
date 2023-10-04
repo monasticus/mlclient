d218b5ea266f4f033e2339d0887fb22d9fb5aee4
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-10-04 09:36:05 +0200

    mlclient #60 - add unit tests for json doc

--- tests/mlclient/clients/test_docs_client.py
+++ tests/mlclient/clients/test_docs_client.py
@@ -32,9 +32,27 @@ def test_read_xml_doc(docs_client):
     builder.with_response_body('<?xml version="1.0" encoding="UTF-8"?>\n<root/>')
     builder.build_get()
 
-    resp = docs_client.read("/some/dir/doc1.xml")
+    resp = docs_client.read(uri)
 
     assert isinstance(resp, ElemTree.ElementTree)
     assert resp.getroot().tag == "root"
     assert resp.getroot().text is None
     assert resp.getroot().attrib == {}
+
+
+@responses.activate
+def test_read_json_doc(docs_client):
+    uri = "/some/dir/doc2.json"
+
+    builder = MLResponseBuilder()
+    builder.with_base_url("http://localhost:8000/v1/documents")
+    builder.with_request_param("uri", uri)
+    builder.with_response_content_type("application/json; charset=utf-8")
+    builder.with_response_status(200)
+    builder.with_response_body('{"root":{"child":"data"}}')
+    builder.build_get()
+
+    resp = docs_client.read(uri)
+
+    assert isinstance(resp, dict)
+    assert resp == {"root": {"child": "data"}}

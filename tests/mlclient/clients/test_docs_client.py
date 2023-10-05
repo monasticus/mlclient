99117a266b8eab9efcd21232b5542eb5fbfeb18e
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-10-05 14:15:33 +0200

    mlclient #60 - cover text document type in docs client

--- tests/mlclient/clients/test_docs_client.py
+++ tests/mlclient/clients/test_docs_client.py
@@ -1,9 +1,10 @@
+import xml.etree.ElementTree as ElemTree
+
 import pytest
 import responses
 
 from mlclient.clients import DocumentsClient
 from tests.tools import MLResponseBuilder
-import xml.etree.ElementTree as ElemTree
 
 
 @pytest.fixture(autouse=True)
@@ -28,8 +29,9 @@ def test_read_xml_doc(docs_client):
     builder.with_base_url("http://localhost:8000/v1/documents")
     builder.with_request_param("uri", uri)
     builder.with_response_content_type("application/xml; charset=utf-8")
+    builder.with_response_header("vnd.marklogic.document-format", "xml")
     builder.with_response_status(200)
-    builder.with_response_body('<?xml version="1.0" encoding="UTF-8"?>\n<root/>')
+    builder.with_response_body(b'<?xml version="1.0" encoding="UTF-8"?>\n<root/>')
     builder.build_get()
 
     resp = docs_client.read(uri)
@@ -48,11 +50,31 @@ def test_read_json_doc(docs_client):
     builder.with_base_url("http://localhost:8000/v1/documents")
     builder.with_request_param("uri", uri)
     builder.with_response_content_type("application/json; charset=utf-8")
+    builder.with_response_header("vnd.marklogic.document-format", "json")
     builder.with_response_status(200)
-    builder.with_response_body('{"root":{"child":"data"}}')
+    builder.with_response_body(b'{"root":{"child":"data"}}')
     builder.build_get()
 
     resp = docs_client.read(uri)
 
     assert isinstance(resp, dict)
     assert resp == {"root": {"child": "data"}}
+
+
+@responses.activate
+def test_read_text_doc(docs_client):
+    uri = "/some/dir/doc3.xqy"
+
+    builder = MLResponseBuilder()
+    builder.with_base_url("http://localhost:8000/v1/documents")
+    builder.with_request_param("uri", uri)
+    builder.with_response_content_type("application/vnd.marklogic-xdmp; charset=utf-8")
+    builder.with_response_status(200)
+    builder.with_response_body(b'xquery version "1.0-ml";\n\nfn:current-date()')
+    builder.with_response_header("vnd.marklogic.document-format", "text")
+    builder.build_get()
+
+    resp = docs_client.read(uri)
+
+    assert isinstance(resp, str)
+    assert resp == 'xquery version "1.0-ml";\n\nfn:current-date()'

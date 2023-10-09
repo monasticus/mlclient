166d161eb11a10581c650562882a9446bae8a48a
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-10-09 16:27:30 +0200

    mlclient #60 - add unit test for multiple uris

--- tests/mlclient/clients/test_docs_client.py
+++ tests/mlclient/clients/test_docs_client.py
@@ -227,3 +227,90 @@ def test_read_binary_doc_uri_list(docs_client):
     assert document.doc_type == DocumentType.BINARY
     assert isinstance(document.content, bytes)
     assert document.content == content
+
+
+# @responses.activate
+def test_multiple_docs(docs_client):
+    uris = [
+        "/some/dir/doc1.xml",
+        "/some/dir/doc2.json",
+        "/some/dir/doc3.xqy",
+        "/some/dir/doc4.zip",
+    ]
+
+    builder = MLResponseBuilder()
+    builder.with_base_url("http://localhost:8000/v1/documents")
+    builder.with_request_param("uri", "/some/dir/doc1.xml")
+    builder.with_request_param("uri", "/some/dir/doc2.json")
+    builder.with_request_param("uri", "/some/dir/doc3.xqy")
+    builder.with_request_param("uri", "/some/dir/doc4.zip")
+    builder.with_response_status(200)
+    builder.with_response_documents_body_part(**{
+        "content-type": "application/zip",
+        "content-disposition": 'attachment; '
+                               'filename="/some/dir/doc4.zip"; '
+                               'category=content; '
+                               'format=binary',
+        "content": b'xquery version "1.0-ml";\n\nfn:current-date()'})
+    builder.with_response_documents_body_part(**{
+        "content-type": "application/xml",
+        "content-disposition": 'attachment; '
+                               'filename="/some/dir/doc1.xml"; '
+                               'category=content; '
+                               'format=xml',
+        "content": '<?xml version="1.0" encoding="UTF-8"?>\n'
+                   '<root><child>data</child></root>'})
+    builder.with_response_documents_body_part(**{
+        "content-type": "application/vnd.marklogic-xdmp",
+        "content-disposition": 'attachment; '
+                               'filename="/some/dir/doc3.xqy"; '
+                               'category=content; '
+                               'format=text',
+        "content": 'xquery version "1.0-ml";\n\nfn:current-date()'})
+    builder.with_response_documents_body_part(**{
+        "content-type": "application/json",
+        "content-disposition": 'attachment; '
+                               'filename="/some/dir/doc2.json"; '
+                               'category=content; '
+                               'format=json',
+        "content": {"root": {"child": "data"}}})
+    builder.build_get()
+
+    docs = docs_client.read(uris)
+
+    assert isinstance(docs, list)
+    assert len(docs) == 4
+
+    xml_docs = list(filter(lambda d: d.uri.endswith(".xml"), docs))
+    assert len(xml_docs) == 1
+    xml_doc = xml_docs[0]
+    assert isinstance(xml_doc, XMLDocument)
+    assert xml_doc.doc_type == DocumentType.XML
+    assert isinstance(xml_doc.content, ElemTree.ElementTree)
+    assert xml_doc.content.getroot().tag == "root"
+    assert xml_doc.content.getroot().text is None
+    assert xml_doc.content.getroot().attrib == {}
+
+    json_docs = list(filter(lambda d: d.uri.endswith(".json"), docs))
+    assert len(json_docs) == 1
+    json_doc = json_docs[0]
+    assert isinstance(json_doc, JSONDocument)
+    assert json_doc.doc_type == DocumentType.JSON
+    assert isinstance(json_doc.content, dict)
+    assert json_doc.content == {"root": {"child": "data"}}
+
+    xqy_docs = list(filter(lambda d: d.uri.endswith(".xqy"), docs))
+    assert len(xqy_docs) == 1
+    xqy_doc = xqy_docs[0]
+    assert isinstance(xqy_doc, StringDocument)
+    assert xqy_doc.doc_type == DocumentType.TEXT
+    assert isinstance(xqy_doc.content, str)
+    assert xqy_doc.content == 'xquery version "1.0-ml";\n\nfn:current-date()'
+
+    zip_docs = list(filter(lambda d: d.uri.endswith(".zip"), docs))
+    assert len(zip_docs) == 1
+    zip_doc = zip_docs[0]
+    assert isinstance(zip_doc, BytesDocument)
+    assert zip_doc.doc_type == DocumentType.BINARY
+    assert isinstance(zip_doc.content, bytes)
+    assert zip_doc.content == b'xquery version "1.0-ml";\n\nfn:current-date()'

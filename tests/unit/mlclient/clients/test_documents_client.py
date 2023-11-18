c7e768ee8a1b7312bc12761521fd2141baf20687
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-11-18 20:59:11 +0100

    mlclient #141 - cover binary document

--- tests/unit/mlclient/clients/test_documents_client.py
+++ tests/unit/mlclient/clients/test_documents_client.py
@@ -2130,7 +2130,7 @@ def test_write_text_document(docs_client):
         {
             "documents": [
                 {
-                    "uri": "/some/dir/doc3.xqy",
+                    "uri": uri,
                     "mime-type": "application/vnd.marklogic-xdmp",
                     "category": ["metadata", "content"],
                 },
@@ -2145,3 +2145,35 @@ def test_write_text_document(docs_client):
     assert len(documents) == 1
     assert documents[0]["uri"] == uri
     assert documents[0]["mime-type"] == "application/vnd.marklogic-xdmp"
+
+
+@responses.activate
+def test_write_binary_document(docs_client):
+    uri = "/some/dir/doc4.zip"
+    content = zlib.compress(b'xquery version "1.0-ml";\n\nfn:current-date()')
+    doc = BinaryDocument(content, uri)
+
+    builder = MLResponseBuilder()
+    builder.with_base_url("http://localhost:8002/v1/documents")
+    builder.with_response_content_type("application/json; charset=utf-8")
+    builder.with_response_header("vnd.marklogic.document-format", "json")
+    builder.with_response_status(200)
+    builder.with_response_body(
+        {
+            "documents": [
+                {
+                    "uri": uri,
+                    "mime-type": "application/zip",
+                    "category": ["metadata", "content"],
+                },
+            ],
+        },
+    )
+    builder.build_post()
+
+    resp = docs_client.create(doc)
+
+    documents = resp["documents"]
+    assert len(documents) == 1
+    assert documents[0]["uri"] == uri
+    assert documents[0]["mime-type"] == "application/zip"

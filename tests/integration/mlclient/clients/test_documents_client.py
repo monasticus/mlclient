a4414a9161c3e0c6a39af8b91e58df84c7563d9e
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-11-30 10:19:01 +0100

    mlclient #153 - cover remaining document types in integration tests

--- tests/integration/mlclient/clients/test_documents_client.py
+++ tests/integration/mlclient/clients/test_documents_client.py
@@ -1,15 +1,61 @@
+import zlib
+
 import pytest
 
 from mlclient.clients import DocumentsClient
 from mlclient.exceptions import MarkLogicError
-from mlclient.model import DocumentType, Document, RawDocument
+from mlclient.mimetypes import Mimetypes
+from mlclient.model import Document, DocumentType, RawDocument
 
 
 def test_manage_xml_document():
     uri = "/some/dir/doc1.xml"
-    content = (b'<?xml version="1.0" encoding="UTF-8"?>\n'
-               b'<root><child>data</child></root>')
+    content = (
+        b'<?xml version="1.0" encoding="UTF-8"?>\n<root><child>data</child></root>'
+    )
     doc = RawDocument(content, uri, DocumentType.XML)
+
+    _assert_document_does_not_exist(uri)
+    try:
+        _create_document(doc)
+        _assert_document_exists_and_confirm_content(doc)
+    finally:
+        _delete_document(uri)
+        _assert_document_does_not_exist(uri)
+
+
+def test_manage_json_document():
+    uri = "/some/dir/doc2.json"
+    content = b'{"root": {"child": "data"}}'
+    doc = RawDocument(content, uri, DocumentType.JSON)
+
+    _assert_document_does_not_exist(uri)
+    try:
+        _create_document(doc)
+        _assert_document_exists_and_confirm_content(doc)
+    finally:
+        _delete_document(uri)
+        _assert_document_does_not_exist(uri)
+
+
+def test_manage_text_document():
+    uri = "/some/dir/doc3.xqy"
+    content = b'xquery version "1.0-ml";\n\nfn:current-date()'
+    doc = RawDocument(content, uri, DocumentType.TEXT)
+
+    _assert_document_does_not_exist(uri)
+    try:
+        _create_document(doc)
+        _assert_document_exists_and_confirm_content(doc)
+    finally:
+        _delete_document(uri)
+        _assert_document_does_not_exist(uri)
+
+
+def test_manage_binary_document():
+    uri = "/some/dir/doc4.zip"
+    content = zlib.compress(b'xquery version "1.0-ml";\n\nfn:current-date()')
+    doc = RawDocument(content, uri, DocumentType.BINARY)
 
     _assert_document_does_not_exist(uri)
     try:
@@ -56,7 +102,7 @@ def _create_document(
         documents = resp["documents"]
         assert len(documents) == 1
         assert documents[0]["uri"] == doc.uri
-        assert documents[0]["mime-type"] == "application/xml"
+        assert documents[0]["mime-type"] == Mimetypes.get_mimetype(doc.uri)
 
 
 def _delete_document(

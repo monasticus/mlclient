8def40e8ae3d1b2810f8a606adc5c548e4d0af3e
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-12-09 16:56:08 +0100

    mlclient #165 - cover custom database

--- tests/unit/mlclient/jobs/test_write_documents_job.py
+++ tests/unit/mlclient/jobs/test_write_documents_job.py
@@ -56,6 +56,59 @@ def test_basic_job():
     assert len(calls) == 1
 
 
+@responses.activate
+def test_job_with_custom_database():
+    docs = _get_test_docs(5)
+
+    builder = MLResponseBuilder()
+    builder.with_base_url("http://localhost:8002/v1/documents")
+    builder.with_request_param("database", "Documents")
+    builder.with_response_content_type("application/json; charset=utf-8")
+    builder.with_response_header("vnd.marklogic.document-format", "json")
+    builder.with_response_status(200)
+    builder.with_response_body(
+        {
+            "documents": [
+                {
+                    "uri": "/some/dir/doc1.xml",
+                    "mime-type": "application/xml",
+                    "category": ["metadata", "content"],
+                },
+                {
+                    "uri": "/some/dir/doc2.xml",
+                    "mime-type": "application/xml",
+                    "category": ["metadata", "content"],
+                },
+                {
+                    "uri": "/some/dir/doc3.xml",
+                    "mime-type": "application/xml",
+                    "category": ["metadata", "content"],
+                },
+                {
+                    "uri": "/some/dir/doc4.xml",
+                    "mime-type": "application/xml",
+                    "category": ["metadata", "content"],
+                },
+                {
+                    "uri": "/some/dir/doc5.xml",
+                    "mime-type": "application/xml",
+                    "category": ["metadata", "content"],
+                },
+            ],
+        },
+    )
+    builder.build_post()
+
+    job = WriteDocumentsJob(thread_count=1, batch_size=5)
+    job.with_client_config(auth_method="digest")
+    job.with_documents_input(docs)
+    job.with_database("Documents")
+    job.start()
+    job.wait_completion()
+    calls = responses.calls
+    assert len(calls) == 1
+
+
 def _get_test_docs(
     count: int,
 ):

7e9bd41c7f6885b551c6fedb14f1e169abbf391e
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-12-16 12:48:34 +0100

    mlclient #183 - fix step definitions import

--- tests/integration/scenarios/mlclient/clients/test_ml_resource_client.py
+++ tests/integration/scenarios/mlclient/clients/test_ml_resource_client.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from pytest_bdd import scenarios
 
-from tests.integration.step_definitions import *
-
 scenarios("../../../features/mlclient/clients/ml_resource_client.feature")
+
+pytest_plugins = [
+    "tests.integration.step_definitions",
+]

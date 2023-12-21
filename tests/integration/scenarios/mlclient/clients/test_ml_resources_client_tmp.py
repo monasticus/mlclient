0fa506b784d9c0fe30f8388f31f5cee24b1d46df
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-12-21 15:44:21 +0100

    mlclient #183 - extract a client's steps to a dedicated module

--- tests/integration/scenarios/mlclient/clients/test_ml_resources_client_tmp.py
+++ tests/integration/scenarios/mlclient/clients/test_ml_resources_client_tmp.py
@@ -5,5 +5,6 @@ from pytest_bdd import scenarios
 scenarios("../../../features/mlclient/clients/ml_resources_client.feature")
 
 pytest_plugins = [
+    "tests.integration.steps.client_steps",
     "tests.integration.steps.step_definitions",
 ]
--- tests/integration/steps/step_definitions.py
+++ tests/integration/steps/step_definitions.py
@@ -1,32 +1,13 @@
 from __future__ import annotations
 
-import datetime
 from time import sleep
 
-from pytest_bdd import given, parsers, then, when
+from pytest_bdd import given, parsers, then
 from requests import Response
 
-from mlclient import MLResourceClient, MLResourcesClient, MLResponseParser
-from mlclient.calls import EvalCall
+from mlclient import MLResourcesClient, MLResponseParser
 
 from .common import parse_step_input
-
-
-@given(
-    parsers.parse("I initialized an {client_type}'s connection"),
-    target_fixture="client",
-)
-def init_client(
-    client_type: str,
-) -> MLResourceClient:
-    if client_type == "MLResourceClient":
-        client = MLResourceClient(auth_method="digest")
-    elif client_type == "MLResourcesClient":
-        client = MLResourcesClient(auth_method="digest")
-    else:
-        raise
-    client.connect()
-    return client
 
 
 @given(
@@ -79,44 +60,6 @@ def wait(
     sleep(int(seconds))
 
 
-@when("I call the EvalCall", target_fixture="response")
-def call_eval(
-    client: MLResourceClient,
-    call_config: dict,
-) -> Response:
-    call = EvalCall(**call_config)
-    return client.call(call)
-
-
-@when("I evaluate the code", target_fixture="response")
-def eval_code(
-    client: MLResourcesClient,
-    call_config: dict,
-) -> Response:
-    return client.eval(**call_config)
-
-
-@when(
-    parsers.parse("I get {logs_type} logs\n{params}"),
-    target_fixture="response",
-)
-def get_logs(
-    client: MLResourcesClient,
-    logs_type: str,
-    params: str,
-) -> Response:
-    params = parse_step_input(params)[0]
-    for time_param in ["start_time", "end_time"]:
-        if time_param in params:
-            params[time_param] = params[time_param].replace(
-                "<today>",
-                str(datetime.date.today()),
-            )
-    params["filename"] = (f"{client.port}_{logs_type.capitalize()}Log.txt",)
-    params["data_format"] = "json"
-    return client.get_logs(**params)
-
-
 @then(parsers.parse("I get a successful response"))
 def verify_response(
     response: Response,
@@ -140,14 +83,6 @@ def verify_multipart_response(
         assert len(expected_parts) == len(parsed_resp)
         for i, expected_part in enumerate(expected_parts):
             assert parsed_resp[i] == expected_part["text"]
-
-
-@then("I close the connection")
-def close_client(
-    client: MLResourceClient,
-):
-    client.disconnect()
-    assert not client.is_connected()
 
 
 @then("I find produced logs")

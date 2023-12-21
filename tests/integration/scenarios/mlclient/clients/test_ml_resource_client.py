e3e6d5c5a0af021e9efbdd14ddb9292f3063c14a
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-12-21 16:08:59 +0100

    mlclient #183 - rename the origin step definitions module

--- tests/integration/scenarios/mlclient/clients/test_ml_resource_client.py
+++ tests/integration/scenarios/mlclient/clients/test_ml_resource_client.py
@@ -7,5 +7,5 @@ scenarios("../../../features/mlclient/clients/ml_resource_client.feature")
 pytest_plugins = [
     "tests.integration.steps.client_steps",
     "tests.integration.steps.responses",
-    "tests.integration.steps.step_definitions",
+    "tests.integration.steps.setup",
 ]

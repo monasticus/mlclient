aa5d9f96d4e4734cbe4e746019d5a5feacbdeef8
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2024-01-18 15:17:28 +0100

    mlclient #184 - fix documents loader tests

--- tests/unit/mlclient/jobs/test_document_loader.py
+++ tests/unit/mlclient/jobs/test_document_loader.py
@@ -5,11 +5,13 @@ from types import GeneratorType
 
 from mlclient.jobs import DocumentsLoader
 from mlclient.model import (
+    BinaryDocument,
     Document,
     DocumentType,
     JSONDocument,
     Metadata,
     RawDocument,
+    TextDocument,
     XMLDocument,
 )
 from tests.utils import resources as resources_utils
@@ -185,75 +187,110 @@ def test_load_documents_and_parse():
     assert type(docs) is GeneratorType
 
     docs = list(docs)
-    assert len(docs) == 7
+    assert len(docs) == 11
 
-    doc_1 = next((doc for doc in docs if doc.uri == "/dir/doc-1.xml"), None)
-    assert doc_1 is not None
-    assert type(doc_1) is XMLDocument
-    assert doc_1.doc_type == DocumentType.XML
-    _assert_xml_content_with_value(doc_1, "value-1")
-    assert doc_1.metadata is None
-    assert doc_1.temporal_collection is None
+    doc_1_1 = next((doc for doc in docs if doc.uri == "/root-1/dir/doc-1.xml"), None)
+    assert doc_1_1 is not None
+    assert type(doc_1_1) is XMLDocument
+    assert doc_1_1.doc_type == DocumentType.XML
+    _assert_xml_content_with_value(doc_1_1, "value-1")
+    assert doc_1_1.metadata is None
+    assert doc_1_1.temporal_collection is None
 
-    doc_2 = next((doc for doc in docs if doc.uri == "/dir/doc-2.json"), None)
-    assert doc_2 is not None
-    assert type(doc_2) is JSONDocument
-    assert doc_2.doc_type == DocumentType.JSON
-    assert doc_2.content == {"root": {"parent": {"child": "value-2"}}}
-    assert doc_2.metadata is not None
-    assert type(doc_2.metadata) is Metadata
-    assert doc_2.metadata.collections() == ["json-doc-2"]
-    assert doc_2.temporal_collection is None
+    doc_1_2 = next((doc for doc in docs if doc.uri == "/root-1/dir/doc-2.json"), None)
+    assert doc_1_2 is not None
+    assert type(doc_1_2) is JSONDocument
+    assert doc_1_2.doc_type == DocumentType.JSON
+    assert doc_1_2.content == {"root": {"parent": {"child": "value-2"}}}
+    assert doc_1_2.metadata is None
+    assert doc_1_2.temporal_collection is None
 
-    doc_3 = next((doc for doc in docs if doc.uri == "/dir/doc-3.xml"), None)
-    assert doc_3 is not None
-    assert type(doc_3) is XMLDocument
-    assert doc_3.doc_type == DocumentType.XML
-    _assert_xml_content_with_value(doc_3, "value-3")
-    assert doc_3.metadata is not None
-    assert type(doc_3.metadata) is Metadata
-    assert doc_3.metadata.collections() == ["xml-doc-3"]
-    assert doc_3.temporal_collection is None
+    doc_1_3 = next((doc for doc in docs if doc.uri == "/root-1/dir/doc-3.xqy"), None)
+    assert doc_1_3 is not None
+    assert type(doc_1_3) is TextDocument
+    assert doc_1_3.doc_type == DocumentType.TEXT
+    assert doc_1_3.content == 'xquery version "1.0-ml";\n\nfn:current-date()'
+    assert doc_1_3.metadata is None
+    assert doc_1_3.temporal_collection is None
 
-    doc_4 = next((doc for doc in docs if doc.uri == "/dir/doc-4.json"), None)
-    assert doc_4 is not None
-    assert type(doc_4) is JSONDocument
-    assert doc_4.doc_type == DocumentType.JSON
-    assert doc_4.content == {"root": {"parent": {"child": "value-4"}}}
-    assert doc_4.metadata is not None
-    assert type(doc_4.metadata) is Metadata
-    assert doc_4.metadata.collections() == ["json-doc-4"]
-    assert doc_4.temporal_collection is None
+    doc_1_4 = next((doc for doc in docs if doc.uri == "/root-1/dir/doc-4.zip"), None)
+    assert doc_1_4 is not None
+    assert type(doc_1_4) is BinaryDocument
+    assert doc_1_4.doc_type == DocumentType.BINARY
+    with zipfile.ZipFile(io.BytesIO(doc_1_4.content)) as zip_file:
+        assert zip_file.namelist() == ["doc-3.xqy"]
+        with zip_file.open("doc-3.xqy") as xqy_file:
+            assert xqy_file.read() == doc_1_3.content.encode()
+    assert doc_1_4.metadata is None
+    assert doc_1_4.temporal_collection is None
 
-    doc_5 = next((doc for doc in docs if doc.uri == "/dir/doc-5.xml"), None)
-    assert doc_5 is not None
-    assert type(doc_5) is XMLDocument
-    assert doc_5.doc_type == DocumentType.XML
-    _assert_xml_content_with_value(doc_5, "value-5")
-    assert doc_5.metadata is not None
-    assert type(doc_5.metadata) is Metadata
-    assert doc_5.metadata.collections() == ["xml-doc-5"]
-    assert doc_5.temporal_collection is None
+    doc_2_1 = next((doc for doc in docs if doc.uri == "/root-2/dir/doc-1.xml"), None)
+    assert doc_2_1 is not None
+    assert type(doc_2_1) is XMLDocument
+    assert doc_2_1.doc_type == DocumentType.XML
+    _assert_xml_content_with_value(doc_2_1, "value-1")
+    assert doc_2_1.metadata is None
+    assert doc_2_1.temporal_collection is None
 
-    doc_6 = next((doc for doc in docs if doc.uri == "/dir/doc-6.json"), None)
-    assert doc_6 is not None
-    assert type(doc_6) is JSONDocument
-    assert doc_6.doc_type == DocumentType.JSON
-    assert doc_6.content == {"root": {"parent": {"child": "value-6"}}}
-    assert doc_6.metadata is not None
-    assert type(doc_6.metadata) is Metadata
-    assert doc_6.metadata.collections() == ["json-doc-6-json-metadata"]
-    assert doc_6.temporal_collection is None
+    doc_2_2 = next((doc for doc in docs if doc.uri == "/root-2/dir/doc-2.json"), None)
+    assert doc_2_2 is not None
+    assert type(doc_2_2) is JSONDocument
+    assert doc_2_2.doc_type == DocumentType.JSON
+    assert doc_2_2.content == {"root": {"parent": {"child": "value-2"}}}
+    assert doc_2_2.metadata is not None
+    assert type(doc_2_2.metadata) is Metadata
+    assert doc_2_2.metadata.collections() == ["json-doc-2"]
+    assert doc_2_2.temporal_collection is None
 
-    doc_7 = next((doc for doc in docs if doc.uri == "/dir/doc-7.xml"), None)
-    assert doc_7 is not None
-    assert type(doc_7) is XMLDocument
-    assert doc_7.doc_type == DocumentType.XML
-    _assert_xml_content_with_value(doc_7, "value-7")
-    assert doc_7.metadata is not None
-    assert type(doc_7.metadata) is Metadata
-    assert doc_7.metadata.collections() == ["xml-doc-7-json-metadata"]
-    assert doc_7.temporal_collection is None
+    doc_2_3 = next((doc for doc in docs if doc.uri == "/root-2/dir/doc-3.xml"), None)
+    assert doc_2_3 is not None
+    assert type(doc_2_3) is XMLDocument
+    assert doc_2_3.doc_type == DocumentType.XML
+    _assert_xml_content_with_value(doc_2_3, "value-3")
+    assert doc_2_3.metadata is not None
+    assert type(doc_2_3.metadata) is Metadata
+    assert doc_2_3.metadata.collections() == ["xml-doc-3"]
+    assert doc_2_3.temporal_collection is None
+
+    doc_2_4 = next((doc for doc in docs if doc.uri == "/root-2/dir/doc-4.json"), None)
+    assert doc_2_4 is not None
+    assert type(doc_2_4) is JSONDocument
+    assert doc_2_4.doc_type == DocumentType.JSON
+    assert doc_2_4.content == {"root": {"parent": {"child": "value-4"}}}
+    assert doc_2_4.metadata is not None
+    assert type(doc_2_4.metadata) is Metadata
+    assert doc_2_4.metadata.collections() == ["json-doc-4"]
+    assert doc_2_4.temporal_collection is None
+
+    doc_2_5 = next((doc for doc in docs if doc.uri == "/root-2/dir/doc-5.xml"), None)
+    assert doc_2_5 is not None
+    assert type(doc_2_5) is XMLDocument
+    assert doc_2_5.doc_type == DocumentType.XML
+    _assert_xml_content_with_value(doc_2_5, "value-5")
+    assert doc_2_5.metadata is not None
+    assert type(doc_2_5.metadata) is Metadata
+    assert doc_2_5.metadata.collections() == ["xml-doc-5"]
+    assert doc_2_5.temporal_collection is None
+
+    doc_2_6 = next((doc for doc in docs if doc.uri == "/root-2/dir/doc-6.json"), None)
+    assert doc_2_6 is not None
+    assert type(doc_2_6) is JSONDocument
+    assert doc_2_6.doc_type == DocumentType.JSON
+    assert doc_2_6.content == {"root": {"parent": {"child": "value-6"}}}
+    assert doc_2_6.metadata is not None
+    assert type(doc_2_6.metadata) is Metadata
+    assert doc_2_6.metadata.collections() == ["json-doc-6-json-metadata"]
+    assert doc_2_6.temporal_collection is None
+
+    doc_2_7 = next((doc for doc in docs if doc.uri == "/root-2/dir/doc-7.xml"), None)
+    assert doc_2_7 is not None
+    assert type(doc_2_7) is XMLDocument
+    assert doc_2_7.doc_type == DocumentType.XML
+    _assert_xml_content_with_value(doc_2_7, "value-7")
+    assert doc_2_7.metadata is not None
+    assert type(doc_2_7.metadata) is Metadata
+    assert doc_2_7.metadata.collections() == ["xml-doc-7-json-metadata"]
+    assert doc_2_7.temporal_collection is None
 
 
 def test_load_document():

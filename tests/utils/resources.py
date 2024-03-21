e49bcfa0ac00bad2d5bbbabafc544fcc2ac0b61a
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2024-03-21 16:13:44 +0100

    mlclient #242 - remove common resources path and support

--- tests/utils/resources.py
+++ tests/utils/resources.py
@@ -9,7 +9,6 @@ _RESOURCES_DIR = "resources"
 _COMMON_RESOURCES_DIR = "common"
 TESTS_PATH = Path(_SCRIPT_DIR).parent.parent
 RESOURCES_PATH = next(TESTS_PATH.glob(_RESOURCES_DIR))
-COMMON_RESOURCES_PATH = next(RESOURCES_PATH.glob(_COMMON_RESOURCES_DIR))
 
 
 def list_resources(
@@ -34,12 +33,6 @@ def get_test_resource_path(
 ) -> str:
     test_resources_path = get_test_resources_path(test_path)
     return next(Path(test_resources_path).glob(resource)).as_posix()
-
-
-def get_common_resource_path(
-    resource: str,
-) -> str:
-    return next(COMMON_RESOURCES_PATH.glob(resource)).as_posix()
 
 
 def get_test_resources_path(

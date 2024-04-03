365f094b656f116ee9390fd06c0d29e48cb6f7c2
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2024-04-03 06:58:51 +0200

    mlclient #185 - setup mlclient envs

--- dev/scripts/get-mimetypes/get-mimetypes.py
+++ dev/scripts/get-mimetypes/get-mimetypes.py
@@ -14,7 +14,7 @@ target_dir_path = next(_SCRIPT_DIR.parent.parent.parent.glob(target_dir))
 target_file_name = "mimetypes.yaml"
 target_file_path = f"{target_dir_path.absolute()}/{target_file_name}"
 
-manager = MLManager("local")
+manager = MLManager("local-single")
 with manager.get_eval_client() as client:
     resp = client.eval(file=str(xqy_file_path))
     with Path(target_file_path).open("w") as target:

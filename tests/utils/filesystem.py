254fa6a69549f3f75953d89b701c77feb636c201
monasticus <tomasz.maciej.aniolowski@gmail.com>
2024-08-09 16:39:42 +0200

    mlclient #186 - create save_rmdir function in tests utils

---
+++ tests/utils/filesystem.py
@@ -0,0 +1,23 @@
+import shutil
+from pathlib import Path
+
+_TESTS_ROOT_NAME = "tests"
+_SCRIPT_PATH: Path = Path(__file__).resolve().absolute()
+
+
+def safe_rmdir(
+    path: str,
+):
+    abs_path = Path(path).resolve().absolute()
+    if not abs_path.is_relative_to(_tests_root()):
+        msg = f"You're trying to remove [{abs_path}] directory. It is not allowed in the safe mode!"
+        raise Exception(msg)
+    shutil.rmtree(abs_path)
+
+
+def _tests_root(
+    path: Path = _SCRIPT_PATH
+) -> Path:
+    if not path.is_dir() or path.name != _TESTS_ROOT_NAME:
+        return _tests_root(path.parent)
+    return path
